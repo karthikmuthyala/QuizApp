dd4dcd2931a023a0743c6f604616790aae8e0b0f
mouni krishna <mounikrishnaa@gmail.com>
2019-03-11 23:35:46 -0500

    added retake button

--- QuizApplication/app/src/main/java/com/example/quizapplication/Score.java
+++ QuizApplication/app/src/main/java/com/example/quizapplication/Score.java
@@ -1,7 +1,10 @@
 package com.example.quizapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Score extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Score extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        Button retake = (Button)findViewById(R.id.retake);
+        retake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ini = new Intent(Score.this, Categories.class);
+                startActivity(ini);
+            }
+        });
     }
 }
