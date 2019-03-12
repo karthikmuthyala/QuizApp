e18ef92d755aa470c7650195a237fcd889614cea
keerthi sree <keerthisree9595@gmail.com>
2019-03-11 21:44:34 -0500

    added Flag,animals,plantsQuizQuestions pages and they are linked.

---
+++ QuizApplication/app/src/main/java/com/example/quizapplication/AnimalsQuizQuestions.java
@@ -0,0 +1,13 @@
+package com.example.quizapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimalsQuizQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animals_quiz_questions);
+    }
+}
--- QuizApplication/app/src/main/java/com/example/quizapplication/Categories.java
+++ QuizApplication/app/src/main/java/com/example/quizapplication/Categories.java
@@ -1,7 +1,10 @@
 package com.example.quizapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Categories extends AppCompatActivity {
 
@@ -9,6 +12,32 @@ public class Categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        Button flag = (Button) findViewById(R.id.button1);
+        Button animals = (Button) findViewById(R.id.button2);
+        Button plants = (Button) findViewById(R.id.button3);
+
+        flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ini = new Intent(Categories.this,FlagQuizQuestions.class);
+                startActivity(ini);
+            }
+        });
+        animals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ini = new Intent(Categories.this,AnimalsQuizQuestions.class);
+                startActivity(ini);
+            }
+        });
+        plants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ini = new Intent(Categories.this,PlantsQuizQuestions.class);
+                startActivity(ini);
+            }
+        });
+
 
     }
 }
